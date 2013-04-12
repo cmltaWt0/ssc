986b0951738c32934f79f4eb4dd4a4a6ea6ced03
Maksim Sokolski <misokolsky@gmail.com>
2013-04-12 12:34:22 +0300

    Avoid absolute path in sessings.py.

--- webssc/webssc/settings.py
+++ webssc/webssc/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_PATH + '/../static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
