c71053704de2c13cb58d15b29d1407abc677e8bd
Maksim Sokolski <misokolsky@gmail.com>
2013-07-04 16:39:02 +0300

    Tabulation behaviour correction. Need to check Google chrome bug.

--- webssc/ssc/static/ssc/js/formRefiner.js
+++ webssc/ssc/static/ssc/js/formRefiner.js
@@ -15,27 +15,23 @@
 
 	next_wrapper = function(n) {
 		function wrapped() {
-			//if (this.value.length == 0 && event.keyIdentifier=="U+0008") {
-			//	this.previousSibling.previousSibling.previousSibling.previousSibling.focus();
-			//}
-			if (this.value.length == n && this.name == "opt7") {
-				document.getElementsByTagName('input')[11].focus();	
+			var value = String(parseInt(this.value));
+			if (value != 0 && value.length == n) {
+				if (this.name == "opt7") {
+					document.getElementsByTagName('input')[11].focus();
+				}
+				else {
+					this.nextSibling.nextSibling.nextSibling.nextSibling.focus();
+				}
 			}
-			else if (parseInt(this.value) != 0 && this.value.length > 0 && this.name == "opt4") {
-				this.nextSibling.nextSibling.nextSibling.nextSibling.focus();				
-			}
-			else if (this.value.length == n) {
-				this.nextSibling.nextSibling.nextSibling.nextSibling.focus();
-			}
-
 		}
 		return wrapped
 	}
 	document.getElementsByTagName('input')[4].onfocus = next_wrapper(1);
 	document.getElementsByTagName('input')[5].onfocus = next_wrapper(1);
-	document.getElementsByTagName('input')[6].onkeyup = next_wrapper(2);
-	document.getElementsByTagName('input')[7].onkeyup = next_wrapper(2);
-	document.getElementsByTagName('input')[8].onkeyup = next_wrapper(2);
+	document.getElementsByTagName('input')[6].oninput = next_wrapper(2);
+	document.getElementsByTagName('input')[7].oninput = next_wrapper(1);
+	document.getElementsByTagName('input')[8].oninput = next_wrapper(2);
 	document.getElementsByTagName('input')[9].onfocus = next_wrapper(1);
-	document.getElementsByTagName('input')[10].onkeyup = next_wrapper(1);
+	document.getElementsByTagName('input')[10].oninput = next_wrapper(1);
 }());
