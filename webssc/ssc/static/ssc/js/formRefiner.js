338a922307c4e95bbbaeefa3df0897b2540e6035
Maksim Sokolski <misokolsky@gmail.com>
2013-07-05 17:17:35 +0300

    Refactor.

--- webssc/ssc/static/ssc/js/formRefiner.js
+++ webssc/ssc/static/ssc/js/formRefiner.js
@@ -1,37 +1,33 @@
 (function() {
-	check_wrapper = function(n) {
-		function wrapped() {
-			var el = document.getElementsByTagName('input')[n];
-			el.checked = true;
-		}
-		return wrapped
-	}
+    check_wrapper = function(n) {
+        function wrapped() {
+	    var el = document.getElementsByTagName('input')[n];
+	    el.checked = true;
+        }
+        return wrapped
+    }
 
-	document.getElementsByTagName('input')[2].onfocus = check_wrapper(1);
-	for (var i=4; i<11; i++) {
-		document.getElementsByTagName('input')[i].onfocus = check_wrapper(3);
-	}
+    document.getElementsByTagName('input')[2].onfocus = check_wrapper(1);
+    for (var i=4; i<11; i++) {
+        document.getElementsByTagName('input')[i].onfocus = check_wrapper(3);
+    }
 
 
-	next_wrapper = function(n) {
-		function wrapped() {
-			var value = String(parseInt(this.value));
-			if (value != 0 && value.length == n) {
-				if (this.name == "opt7") {
-					document.getElementsByTagName('input')[11].focus();
-				}
-				else {
-					this.nextSibling.nextSibling.nextSibling.nextSibling.focus();
-				}
-			}
-		}
-		return wrapped
-	}
-	document.getElementsByTagName('input')[4].onfocus = next_wrapper(1);
-	document.getElementsByTagName('input')[5].onfocus = next_wrapper(1);
-	document.getElementsByTagName('input')[6].oninput = next_wrapper(2);
-	document.getElementsByTagName('input')[7].oninput = next_wrapper(1);
-	document.getElementsByTagName('input')[8].oninput = next_wrapper(2);
-	document.getElementsByTagName('input')[9].onfocus = next_wrapper(1);
-	document.getElementsByTagName('input')[10].oninput = next_wrapper(1);
+    next_wrapper = function(n) {
+        function wrapped() {
+            var value = String(parseInt(this.value));
+            if (value != 0 && value.length == n) {
+                document.getElementsByName('opt'+
+                         (parseInt(this.name.slice(-1))+1))[0].focus();
+            }
+        }
+        return wrapped
+    }
+    document.getElementsByTagName('input')[4].onfocus = next_wrapper(1);
+    document.getElementsByTagName('input')[5].onfocus = next_wrapper(1);
+    document.getElementsByTagName('input')[6].oninput = next_wrapper(2);
+    document.getElementsByTagName('input')[7].oninput = next_wrapper(1);
+    document.getElementsByTagName('input')[8].oninput = next_wrapper(2);
+    document.getElementsByTagName('input')[9].onfocus = next_wrapper(1);
+    document.getElementsByTagName('input')[10].oninput = next_wrapper(1);
 }());
--- webssc/ssc/static/ssc/js/hrefAddClass.js
+++ webssc/ssc/static/ssc/js/hrefAddClass.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-	var path = location.pathname;
-	var links = document.querySelectorAll('.nav > li > a');
+    var path = location.pathname;
+    var links = document.querySelectorAll('.nav > li > a');
 
-	for (var i=0; i < links.length; i++) {
-		if (path == links[i].pathname) {
-			links[i].parentNode.className='active';
-		}
-	}
+    for (var i=0; i < links.length; i++) {
+        if (path == links[i].pathname) {
+            links[i].parentNode.className='active';
+        }
+    }
 };
 
