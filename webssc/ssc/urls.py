e7415f226918d72ec70344680a7503431393c5b0
Maksim Sokolski <misokolsky@gmail.com>
2013-08-21 13:28:04 +0300

    Fix handler bug in ssc/urls.py

--- webssc/ssc/urls.py
+++ webssc/ssc/urls.py
@@ -3,10 +3,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'ssc.views.simple_http_handler', {'xml': False}, name='http'),
-    url(r'^xml/$', 'ssc.views.simple_http_handler', {'xml': True}, name='http'),
+    url(r'^xml/$', 'ssc.views.simple_http_handler', {'xml': True}, name='http_xml')
 
     url(r'^ajax/$', 'ssc.views.ajax_http_handler', {'xml': False}, name='ajax'),
-    url(r'^ajax/xml/$', 'ssc.views.ajax_http_handler', {'xml': True}, name='ajax'),
+    url(r'^ajax/xml/$', 'ssc.views.ajax_http_handler', {'xml': True}, name='ajax_xml'),
 
     url(r'^accounts/login/$', 'ssc.views.user_login', name='login'),
     url(r'^accounts/login/welcome/$', TemplateView.as_view(template_name='ssc/welcome.html'), name='welcome'),
