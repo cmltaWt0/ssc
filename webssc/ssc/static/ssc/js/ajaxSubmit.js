7e7a79c33e872f7184bfb40e42cdb2a7c34530a1
Maksim Sokolski <misokolsky@gmail.com>
2013-09-02 14:37:21 +0300

    Refinment readability.

--- webssc/ssc/static/ssc/js/ajaxSubmit.js
+++ webssc/ssc/static/ssc/js/ajaxSubmit.js
@@ -1,51 +1,52 @@
 function getForm() {
-	var name = document.getElementById('login_name').name;
-	var value = document.getElementById('login_name').value;
-	var data = [];
-	name = encodeURIComponent(name);
-	value = encodeURIComponent(value);
-	data.push(name + "=" + value);
-	return data.join("&");
+    var name = document.getElementById('login_name').name;
+    var value = document.getElementById('login_name').value;
+    var data = [];
+    name = encodeURIComponent(name);
+    value = encodeURIComponent(value);
+    data.push(name + "=" + value);
+    return data.join("&");
 }
 
 
 function getCSRFToken(name) {
-	var cookie = null;
-	if (document.cookie && document.cookie != '') {
+    var cookie = null;
+    if (document.cookie && document.cookie != '') {
         var allcookies = document.cookie.split('; ');
         for (i in allcookies) {
-        	if (allcookies[i].split('=')[0] == name) {
-        		cookie = allcookies[i].split('=')[1];
-        		break;
-        	}
-
+            if (allcookies[i].split('=')[0] == name) {
+                cookie = allcookies[i].split('=')[1];
+                break;
+            }
         }
-	}
-	return cookie;
+    }
+    return cookie;
 }
 
 
 function ajaxRequest(xml) {
-	var request = new XMLHttpRequest();
-        xml = xml || false;
-        xml ? request.open("POST", "/ssc/ajax/xml/")
-            : request.open("POST", "/ssc/ajax/");
-        request.setRequestHeader("Content-Type",
-                                 "application/x-www-form-urlencoded");
-        request.setRequestHeader("X-CSRFToken", getCSRFToken('csrftoken'))
-	request.onreadystatechange = function() {
-		if (request.readyState === 4 && request.status === 200) {
-			document.getElementById('login_name').
-                                 value = request.responseText;
-		}
-	};
-	request.send(getForm());
+    var request = new XMLHttpRequest();
+    xml = xml || false;
+    xml ? request.open("POST", "/ssc/ajax/xml/")
+        : request.open("POST", "/ssc/ajax/");
+    request.setRequestHeader("Content-Type",
+                             "application/x-www-form-urlencoded");
+    request.setRequestHeader("X-CSRFToken", getCSRFToken('csrftoken'))
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+	    document.getElementById('login_name').
+                                    value = request.responseText;
+        }
+    };
+    
+    request.send(getForm());
 }
 
 
 document.getElementById('ajax_submit').onclick = function () {
-	ajaxRequest();
-	return false;
+    ajaxRequest();
+    return false;
 }
 
 
